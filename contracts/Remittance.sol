1044a0271f3bdbdb31e95df0ef68dd98c7c1cd48
SvenRei <sven.reimers@tuhh.de>
2020-04-21 21:57:19 +0200

    adding cancel function

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -14,54 +14,70 @@ contract Remittance is Killable{
     uint constant maxDeadline = 7 * 86400 / 15;
 
     //struct for setting the data
-    struct Dataset{
+    struct Remittance {
       uint amount;
       uint maxDate;
       address sender;
     }
-
     //Set up events
-    event LogDeploy(address indexed sender, uint deadline, uint amount, bytes32 hash);
-    event LogWithdraw(address indexed sender, bytes32 hash);
+       event LogDeploy(address indexed sender, uint deadline, uint amount, bytes32 hash);
+       event LogWithdraw(address indexed sender, bytes32 hash);
+       event LogCancel(address indexed sender, bytes32 hash, uint amount, uint  deadline);
 
     //setting up a mapping
-    mapping(bytes32 => Dataset) public data;
+    mapping(bytes32 => Remittance) public remittances;
+
+    constructor() public{
+    }
 
   //setting up a function to hash inside the contract, because it is not clear which hash function the participants use
-  //setting up the secret with the address of the address of the exchange shop (one password less), the passwort from bob and the address of this contract(for using it only once)
-  function hash(address exchange, bytes32 passwort) public view returns(bytes32 hash){
-    hash = keccak256(abi.encodePacked(exchange, passwort, address(this)));
+  //setting up the secret with the address of the address of the exchange shop (one password less), the password from bob and the address of this contract(for using it only once)
+  function hash(address exchange, bytes32 password) public view returns(bytes32 hash){
+    hash = keccak256(abi.encodePacked(exchange, password, address(this)));
   }
 
   //function to set up the riddle with the hash and the date
-  function deploy(bytes32 hash, uint maxDate) public payable {
+  function sendRemittance(bytes32 hash, uint maxDate) public payable {
     //amount from msg.sender
     uint amount = msg.value;
 
     require(amount > 0, "greater than zero");
     require(maxDate < maxDeadline, "Deadline!");
     //using the storage // https://medium.com/cryptologic/memory-and-storage-in-solidity-4052c788ca86
-    Dataset storage d = data[hash];
+    Remittance storage r = remittances[hash];
     //checking that the pw or acc isn't already set
-    require(d.sender == address(0), "wrong pw or acc");
-    d.sender = msg.sender;
-    d.amount = amount;
-    d.maxDate = maxDate;
+    require(r.sender == address(0), "wrong pw or acc");
+    r.sender = msg.sender;
+    r.amount = amount;
+    r.maxDate = maxDate;
     //event
     emit LogDeploy(msg.sender, maxDate, msg.value, hash);
   }
+
   //withdraw the funds like in the splitter project but with .call!
   function withdraw(bytes32 password) public {
     bytes32 hash = hash(msg.sender, password);
-    Dataset storage d = data[hash];
-    uint withdrawAmount = d.amount;
-    d.amount = 0;
-    d.maxDate = 0;
+    Remittance storage r = remittances[hash];
+    uint withdrawAmount = r.amount;
+    r.amount = 0;
+    r.maxDate = 0;
     //event
     emit LogWithdraw(msg.sender, hash);
     (bool success, ) = msg.sender.call.value(withdrawAmount)("");
-       require(success, "Transfer failed.");
+       require(success, "Transfer failer");
+  }
 
+  function cancelRemittance(bytes32 hash) public payable whenAlive {
+    require(remittances[hash].sender == msg.sender, "Caller did not sendRemittance");
+    require(remittances[hash].maxDate > maxDeadline, "has not yet expired");
+    Remittance storage r = remittances[hash];
+    emit LogCancel(msg.sender, hash, r.amount, r.maxDate);
+    uint cancelAmount = r.amount;
+    remittances[hash].amount = 0;
+    remittances[hash].maxDate = 0;
+
+    (bool success, ) = msg.sender.call.value(cancelAmount)("");
+       require(success, "Transfer failer");
   }
 
 
