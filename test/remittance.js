ddcccfc5fc0e8d8031f89720d17b2ea93443463f
SvenRei <sven.reimers@tuhh.de>
2020-05-06 15:11:36 +0200

    a lot of renaming

--- test/remittance.js
+++ test/remittance.js
@@ -14,7 +14,7 @@ contract('Remittance', (accounts) => {
 
   //set pw1
   const pw1 = "beer1234";
-
+  const pw2 = "test1234";
   // build up a new Splitter contract before each test
   const SECONDS_IN_DAY = 86400
 
@@ -35,28 +35,28 @@ contract('Remittance', (accounts) => {
     await contractInstance.pause({from: sender});
     const killObj = await contractInstance.kill({ from: sender });
     const { logs } = killObj;
-    const checkEvent = killObj.logs[0];
+    const killEvent = killObj.logs[0];
     truffleAssert.eventEmitted(killObj, "LogKilled");
-    assert.strictEqual(checkEvent.args.sender, sender, "not the owner");
+    assert.strictEqual(killEvent.args.sender, sender, "not the owner");
    });
 
   it("test: LogRefunds-event should be emitted", async() => {
    const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
 
    const amount = web3.utils.toWei("1", "Gwei");
-   const maxDate = 23040;
+   const deadline = 23040;
    //call the contract from sender
-   const deployObject = await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+   await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
 
    await contractInstance.pause({ from: sender });
    await contractInstance.kill({ from: sender });
    const refundObj = await contractInstance.safeFunds({ from: sender });
 
    const { logs } = refundObj;
-   const checkEvent = refundObj.logs[0];
+   const refundEvent = refundObj.logs[0];
    truffleAssert.eventEmitted(refundObj, "LogRefunds");
-   assert.strictEqual(checkEvent.args.sender, sender, "not the owner");
-   assert.strictEqual(checkEvent.args.refunds.toString(), amount.toString(), "not the right refund");
+   assert.strictEqual(refundEvent.args.sender, sender, "not the owner");
+   assert.strictEqual(refundEvent.args.refunds.toString(), amount.toString(), "not the right refund");
  });
 
   it("test: LogDeploy-event should be emitted", async() => {
@@ -64,16 +64,21 @@ contract('Remittance', (accounts) => {
     const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
 
     const amount = web3.utils.toWei("1", "Gwei");
-    const maxDate = 23040;
+    const deadline = 23040;
     //call the contract from sender
-    const deployObject = await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
-    const { logs } = deployObject;
-    const checkEvent = deployObject.logs[0];
-    truffleAssert.eventEmitted(deployObject, "LogDeploy");
-    assert.strictEqual(checkEvent.args.sender, sender, "sender isn't right");
-    assert.strictEqual(checkEvent.args.deadline.toString(), maxDate.toString(), "latestBlock is not right");
-    assert.strictEqual(checkEvent.args.amount.toString(),amount.toString() , "amount is not right");
-    assert.strictEqual(checkEvent.args.hash, hash, "hash problem");
+    const remittanceObject = await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
+
+    const getBlock = await web3.eth.getBlock('latest');
+    const getTime = getBlock.timestamp + deadline; //get time + deadline for checking LogDeploy-event
+
+    const { logs } = remittanceObject;
+    const remittanceEvent = remittanceObject.logs[0];
+    truffleAssert.eventEmitted(remittanceObject, "LogDeploy");
+
+    assert.strictEqual(remittanceEvent.args.sender, sender, "sender isn't right");
+    assert.strictEqual(remittanceEvent.args.deadline.toString(), getTime.toString(), "latestBlock is not right");
+    assert.strictEqual(remittanceEvent.args.amount.toString(),amount.toString() , "amount is not right");
+    assert.strictEqual(remittanceEvent.args.hash, hash, "hash problem");
    });
 
   it("test: deploy 2 funds in the contract | calling sendRemittance from different accounts", async() => {
@@ -81,26 +86,26 @@ contract('Remittance', (accounts) => {
      const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
 
      const amount = web3.utils.toWei("1", "Gwei");
-     const maxDate = 23040;
+     const deadline = 23040;
      //call the contract from sender
-     const deployObject = await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
-     const { logs } = deployObject;
-     const checkEvent = deployObject.logs[0];
-     truffleAssert.eventEmitted(deployObject, "LogDeploy");
-     assert.strictEqual(checkEvent.args.amount.toString(),amount.toString() , "amount is not right");
+     const remittanceObject = await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
+     const { logs } = remittanceObject;
+     const remittanceEvent = remittanceObject.logs[0];
+     truffleAssert.eventEmitted(remittanceObject, "LogDeploy");
+     assert.strictEqual(remittanceEvent.args.amount.toString(),amount.toString() , "amount is not right");
      //truffleAssert.prettyPrintEmittedEvents(deployObject);
 
      //setting the amount
-     const hash1 = await contractInstance.hash(two, web3.utils.toHex(pw1));
+     const hash1 = await contractInstance.hash(two, web3.utils.toHex(pw2));
 
      const amount1 = web3.utils.toWei("2", "Gwei");
-     const maxDate1 = 24040;
+     const deadline1 = 24040;
      //call the contract from one
-     const deployObject1 = await contractInstance.sendRemittance(hash1, maxDate1 , {from: one, value: amount1});
-     const { logs1 } = deployObject1;
-     const checkEvent1 = deployObject1.logs[0];
-     truffleAssert.eventEmitted(deployObject1, "LogDeploy");
-     assert.strictEqual(checkEvent1.args.amount.toString(),amount1.toString() , "amount is not right");
+     const remittanceObject1 = await contractInstance.sendRemittance(hash1, deadline1 , {from: one, value: amount1});
+     const { logs1 } = remittanceObject1;
+     const remittanceEvent1 = remittanceObject1.logs[0];
+     truffleAssert.eventEmitted(remittanceObject1, "LogDeploy");
+     assert.strictEqual(remittanceEvent1.args.amount.toString(),amount1.toString() , "amount is not right");
      //truffleAssert.prettyPrintEmittedEvents(deployObject1);
     });
 
@@ -108,11 +113,11 @@ contract('Remittance', (accounts) => {
       //setting the amount
       const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
       const amount = web3.utils.toWei("1", "Gwei");
-      const maxDate = 23040;
+      const deadline = 23040;
       //call the contract from sender
-      deployObject = await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+      deployObject = await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
       await contractInstance.withdraw(web3.utils.toHex(pw1) , {from: one});
-      await truffleAssert.fails(contractInstance.sendRemittance(hash, maxDate,
+      await truffleAssert.fails(contractInstance.sendRemittance(hash, deadline,
            {from: sender, value: amount}));
      });
 
@@ -121,19 +126,19 @@ contract('Remittance', (accounts) => {
      const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
 
      const amount = web3.utils.toWei("1", "Gwei");
-     const maxDate = 23040;
+     const deadline = 23040;
      //call the contract from sender
-     await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+     await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
 
      //getting the balance before withdraw
      const balanceBefore = await web3.eth.getBalance(one);
 
      const withdrawObject = await contractInstance.withdraw(web3.utils.toHex(pw1) , {from: one});
      const { logs } = withdrawObject;
-     const checkEvent = withdrawObject.logs[0];
+     const withdrawEvent = withdrawObject.logs[0];
      truffleAssert.eventEmitted(withdrawObject, "LogWithdraw");
-     assert.strictEqual(checkEvent.args.sender, one, "sender isn't right");
-     assert.strictEqual(checkEvent.args.hash, hash, "hash problem");
+     assert.strictEqual(withdrawEvent.args.sender, one, "sender isn't right");
+     assert.strictEqual(withdrawEvent.args.hash, hash, "hash problem");
 
      const tx = await web3.eth.getTransaction(withdrawObject.tx);
      //getting the receipt for calculating gasCost
@@ -154,9 +159,9 @@ contract('Remittance', (accounts) => {
       //setting the amount
       const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
       const amount = web3.utils.toWei("1", "Gwei");
-      const maxDate = 23040;
+      const deadline = 23040;
       //call the contract from sender
-      await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+      await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
 
       await truffleAssert.fails(contractInstance.cancelRemittance(hash, {from: sender}));
 
@@ -166,29 +171,20 @@ contract('Remittance', (accounts) => {
       //setting the amount
       const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
       const amount = web3.utils.toWei("1", "Gwei");
-      const maxDate = 23040;
+      const deadline = 23040;
       //call the contract from sender
-      await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+      await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
       //console.log(web3.eth.getBlock.toString());
       await helper.advanceTime(SECONDS_IN_DAY * 11);
       await helper.advanceBlock()
 
-      /*
-      const blockAfter = await web3.eth.getBlock('latest')
-      const blockNumberAfter = blockAfter.number
-      const timeAfter = blockAfter.timestamp
-         console.log(timeAfter);
-
-      const maxDateAfter = timeAfter  + maxDate;
-      */
-
       const cancelObject = await contractInstance.cancelRemittance(hash , {from: sender});
       const { logs } = cancelObject;
-      const checkEvent = cancelObject.logs[0];
+      const cancelEvent = cancelObject.logs[0];
       truffleAssert.eventEmitted(cancelObject, "LogCancel");
-      assert.strictEqual(checkEvent.args.sender, sender, "sender isn't right");
-      assert.strictEqual(checkEvent.args.hash, hash, "hash problem");
-      assert.strictEqual(checkEvent.args.amount.toString(),amount.toString() , "amount is not right");
+      assert.strictEqual(cancelEvent.args.sender, sender, "sender isn't right");
+      assert.strictEqual(cancelEvent.args.hash, hash, "hash problem");
+      assert.strictEqual(cancelEvent.args.amount.toString(),amount.toString() , "amount is not right");
 
      });
 
@@ -196,9 +192,9 @@ contract('Remittance', (accounts) => {
        //setting the amount
        const hash = await contractInstance.hash(one, web3.utils.toHex(pw1));
        const amount = web3.utils.toWei("1", "Gwei");
-       const maxDate = 23040;
+       const deadline = 23040;
        //call the contract from sender
-       await contractInstance.sendRemittance(hash, maxDate , {from: sender, value: amount});
+       await contractInstance.sendRemittance(hash, deadline , {from: sender, value: amount});
        await contractInstance.withdraw(web3.utils.toHex(pw1) , {from: one});
 
        //console.log(web3.eth.getBlock.toString());
