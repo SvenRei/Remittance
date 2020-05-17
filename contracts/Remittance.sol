6f84592621d486418d777d5df836b65eaaf7eca7
SvenRei <sven.reimers@tuhh.de>
2020-05-17 21:35:09 +0200

    whenAlive added; Changed error messages and events

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -21,9 +21,9 @@ contract Remittance is Killable{
       address sender;
     }
     //Set up events
-       event LogDeploy(address indexed sender, uint deadline, uint amount, bytes32 hash);
-       event LogWithdraw(address indexed sender, bytes32 hash);
-       event LogCancel(address indexed sender, bytes32 hash, uint amount, uint  deadline);
+       event LogDeploy(bytes32 hash, address indexed sender, uint deadline, uint amount);
+       event LogWithdraw(bytes32 hash, address indexed sender);
+       event LogCancel(bytes32 hash, address indexed sender, uint amount, uint  deadline);
 
     //setting up a mapping
     mapping(bytes32 => Remittance) public remittances;
@@ -34,21 +34,21 @@ contract Remittance is Killable{
   //setting up a function to hash inside the contract, because it is not clear which hash function the participants use
   //setting up the secret with the address of the address of the exchange shop (one password less), the password from bob and the address of this contract(for using it only once)
   function hash(address exchange, bytes32 password) public view returns(bytes32 hash){
-    require(exchange != address(0x0), "the addresses can't be 0x0");
+    require(exchange != address(0x0), "The address can't be 0x0");
     hash = keccak256(abi.encodePacked(exchange, password, address(this)));
   }
 
   //function to set up the riddle with the hash and the date
-  function sendRemittance(bytes32 hash, uint remittancePeriod) public payable {
+  function sendRemittance(bytes32 hash, uint remittancePeriod) public payable whenAlive {
     //amount from msg.sender
     uint amount = msg.value;
 
-    require(amount > 0, "greater than zero");
-    require(remittancePeriod <= maxRemittancePeriod, "Deadline!");
+    require(amount > 0, "The amount must be greater than 0");
+    require(remittancePeriod <= maxRemittancePeriod, "maxRemittancePeriod must not be exceeded");
     //using the storage // https://medium.com/cryptologic/memory-and-storage-in-solidity-4052c788ca86
     Remittance storage r = remittances[hash];
     //checking that the pw or acc isn't already set
-    require(r.sender == address(0), "wrong pw or acc");
+    require(r.sender == address(0), "Wrong pw or acc");
 
     uint deadline = now.add(remittancePeriod); //for Setting the right period!
 
@@ -56,20 +56,20 @@ contract Remittance is Killable{
     r.amount = amount;
     r.deadline = deadline;
     //event
-    emit LogDeploy(msg.sender, deadline, msg.value, hash);
+    emit LogDeploy(hash, msg.sender, deadline, msg.value);
   }
 
   //withdraw the funds like in the splitter project but with .call!
-  function withdraw(bytes32 password) public {
+  function withdraw(bytes32 password) public whenAlive{
     bytes32 hash = hash(msg.sender, password);
     Remittance storage r = remittances[hash];
     uint withdrawAmount = r.amount;
-    require(now <= r.deadline, "he got one week");
-    require(withdrawAmount > 0,"nothing to withdraw");
+    require(now <= r.deadline, "The deadline has been exceeded");
+    require(withdrawAmount > 0,"Nothing to withdraw");
     r.amount = 0;
     r.deadline = 0;
     //event
-    emit LogWithdraw(msg.sender, hash);
+    emit LogWithdraw(hash, msg.sender);
     (bool success, ) = msg.sender.call.value(withdrawAmount)("");
        require(success, "Transfer failed");
   }
@@ -79,10 +79,10 @@ contract Remittance is Killable{
     Remittance storage r = remittances[hash];
     uint cancelAmount = r.amount;
     uint deadline = r.deadline;
-    require(r.sender == msg.sender, "wrong msg.sender");
-    require(deadline < now, "the deadline has not yet expired");
-    require(cancelAmount > 0,"nothing to withdraw");
-    emit LogCancel(msg.sender, hash, cancelAmount, deadline);
+    require(r.sender == msg.sender, "The msg.sender address is not correct");
+    require(deadline < now, "The deadline has not yet expired");
+    require(cancelAmount > 0,"Nothing to withdraw");
+    emit LogCancel(hash, msg.sender, cancelAmount, deadline);
     r.amount = 0;
     r.deadline = 0;
 
